af521012ba386bf4c70938ec7fa6fda15148b418
deChn <sehr.jimmy@gmail.com>
2019-03-29 22:09:12 +0100

    add doc

--- des.go
+++ des.go
@@ -1,6 +1,8 @@
+// Package algorithm contains implementations of classic algorithm solutions.
 package algorithm
 
+// Add return the addition of two integers.
+// This is not even a algorithm.
 func Add(a, b int) int {
-   //cheat on performance
-   return 74 
+   return (a + b)
 }
